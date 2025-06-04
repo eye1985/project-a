5ced8b194bbc09c98d7e2a6a7c4b9de7629dd556
erik <eye1985@gmail.com>
2025-06-04 15:33:08 +0200

    - Update test to use rel path

--- internal/email/repository_test.go
+++ internal/email/repository_test.go
@@ -2,22 +2,16 @@ package email
 
 import (
 	"context"
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"log"
 	"net"
-	"os"
 	"project-a/internal/database"
 	"testing"
 )
 
 func TestWithPostgresSQL(t *testing.T) {
-	if err := godotenv.Load("../../.env"); err != nil {
-		log.Println("No .env file found")
-	}
-
 	ctx := context.Background()
 
 	dbName := "testing"
@@ -25,7 +19,7 @@ func TestWithPostgresSQL(t *testing.T) {
 	dbPass := "password"
 
 	postgresContainer, err := postgres.Run(
-		ctx, "postgres:16.8-alpine",
+		ctx, "postgres:16.9-alpine",
 		postgres.WithDatabase(dbName),
 		postgres.WithUsername(dbUser),
 		postgres.WithPassword(dbPass),
@@ -48,9 +42,7 @@ func TestWithPostgresSQL(t *testing.T) {
 
 	connStr, err := postgresContainer.ConnectionString(ctx, "sslmode=disable")
 	require.NoError(t, err)
-	root := os.Getenv("PROJECT_ROOT")
-
-	database.Migrate(connStr, root)
+	database.Migrate(connStr, "../..")
 	err = postgresContainer.Snapshot(ctx, postgres.WithSnapshotName("sentEmail-snapshot"))
 	require.NoError(t, err)
 
